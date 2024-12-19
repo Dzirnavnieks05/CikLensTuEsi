506a92f202ccd1efdac7a71632c9f766a2752f62
Dzirnavnieks05 <118040998+Dzirnavnieks05@users.noreply.github.com>
2024-12-19 22:30:36 +0200

    Tiek parādīta statistika
    Spēles beigās tiek aprēķinātas laika un attāluma vidējās vērtības un standartnovirzes. Vērtības tiek attēlotas histogrammā.
    
    Attāluma histogrammā ir mazliet dīvaini ar Gausa sadalījumu - izskatās augstāks, nekā vajadzētu būt.

--- __init__.py
+++ __init__.py
@@ -6,8 +6,15 @@ programmu, atveras grafiskais logs. Šajā grafiskajā logā lietotājs var spē
 spēles beigšanas, Python faila mapē saglabājas PDF grafiki ar aprakstīto statistisko analīzi.'''
 
 import tkinter
+import matplotlib.axes
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.axes import Axes
+
 from random import randint
 from time import time
+
+sad_gauss = lambda x, mu, s: np.exp(-np.square((x-mu)/s)/2) / np.sqrt(2*s)
 
 #Galvenie mainīgie
 GARUMS, PLATUMS = 800, 600
@@ -28,6 +35,9 @@ def sāktSpēli():
     #Pievieno audeklu, kur viss tiek zīmēts:
     audekls.place(relx=0.5,rely=0.5, anchor='center')
 
+    #Notīra un paslēpj grafiku:
+    ax_laiks.clear()
+    ax_att.clear()
     
     audekls.bind('<Button-1>', gājiens)
 
@@ -44,7 +54,7 @@ def gājiens(notikums):
         global aplis_x, aplis_y, izpildītas_r, laiks
         laiki.append(time()-laiks)
         laiks = time()
-        attālumi.append((notikums.x-aplis_x)**2+(notikums.y-aplis_y)**2)
+        attālumi.append(np.sqrt((notikums.x-aplis_x)**2+(notikums.y-aplis_y)**2))
         
         aplis_x = randint(RĀDIUSS, GARUMS -RĀDIUSS)
         aplis_y = randint(RĀDIUSS, PLATUMS-RĀDIUSS)
@@ -54,10 +64,58 @@ def gājiens(notikums):
         )
         izpildītas_r += 1
 
+        #Spēles beigas
         if izpildītas_r==REIZES:
-            print('Viss')
-            print(laiki)
-            print(attālumi)
+            #Sarēķina statistiku:
+            N_stabiņi = int(2*REIZES**(1/3)+1)#Stabiņu skaits pēc Rīsa likuma
+            
+            S_laiks = REIZES*(np.max(laiki)-np.min(laiki))/N_stabiņi#Gausa sadalījuma izstiepšana
+            laiki_ = np.linspace(np.min(laiki), np.max(laiki))#Smukākam Gausa sadalījumam
+            t_vid = np.average(laiki)
+            t_std_nov = np.std(laiki)
+
+            S_att = REIZES*(np.max(attālumi)-np.min(attālumi))/N_stabiņi#Gausa sadalījuma izstiepšana
+            attālumi_ = np.linspace(np.min(attālumi), np.max(attālumi))#Smukākam Gausa sadalījumam
+            a_vid = np.average(attālumi)
+            a_std_nov = np.std(attālumi)
+
+            #Liekam šeit, jo "Axes" objekti tiek pilnībā notīrīti
+            ax_att.set(
+                title='Attālumi'
+            )
+            ax_laiks.set(
+                title='Laiki'
+            )
+
+            #Laiks
+            ax_laiks.hist(laiki, bins=N_stabiņi)
+            ax_laiks.annotate(
+                text=f'Vidēji: {t_vid: .2f} s', 
+                xy=(0.65, 0.9),
+                xycoords='axes fraction'
+            )
+            ax_laiks.annotate(
+                text=f'Std. nov.: {t_std_nov: .2f} s', 
+                xy=(0.65, 0.85),
+                xycoords='axes fraction'
+            )
+            ax_laiks.plot(laiki_, S_laiks*sad_gauss(laiki_, t_vid, t_std_nov))
+            #Attālums
+            ax_att.hist(attālumi, bins=N_stabiņi)
+            ax_att.annotate(
+                text=f'Vidēji: {a_vid: .2f} px', 
+                xy=(0.65, 0.9),
+                xycoords='axes fraction'
+            )
+            ax_att.annotate(
+                text=f'Std. nov.: {a_std_nov: .2f} px', 
+                xy=(0.65, 0.85),
+                xycoords='axes fraction'
+            )
+            ax_att.plot(attālumi_, S_att*sad_gauss(attālumi_, a_vid, a_std_nov))
+            
+            fig.show()
+
             attiestīt()
 
 #Spēles logs:
@@ -84,6 +142,14 @@ laiks = 0
 laiki = []
 attālumi = []
 
+#Histrogramma
+ax_att: Axes
+ax_laiks: Axes
+fig, (ax_laiks,ax_att) = plt.subplots(
+    ncols=2,
+    num='Cik lēns tu esi? Apkopojums',
+    figsize=(10,6)
+)
 
 
 attiestīt()
